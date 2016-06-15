8289c5a00d25e90c6482cee84a31c93e9b9ef75f
Adam Stiff <stiff.4@osu.edu>
2016-06-15 17:29:43 -0400

    Removing unnecessary junk from version control
    Including binary directories, IDE files, and stuff to be replaced.

--- CRF/Debug/src/CRF_LogMath.d
+++
@@ -1,6 +0,0 @@
-src/CRF_LogMath.d src/CRF_LogMath.o: ../src/CRF_LogMath.cpp \
-  ../src/CRF_LogMath.h ../src/CRF_Utils.h
-
-../src/CRF_LogMath.h:
-
-../src/CRF_Utils.h:
--- CRFFstAlign/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lpthread -lCRF -lquicknet3 -lptcblas -lcblas -latlas -lfst
-
